5dbcfcb32b3ed0661815fef012917416acad3e89
Nihar Thakkar <nihartar@gmail.com>
2018-08-10 09:20:27 +0530

    Disable BottomNavigationView shift mode, show menu item titles and downgrade compile SDK version

--- app/src/main/java/io/eelo/appinstaller/MainActivity.kt
+++ app/src/main/java/io/eelo/appinstaller/MainActivity.kt
@@ -2,11 +2,40 @@ package io.eelo.appinstaller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
+import android.support.design.widget.BottomNavigationView
+import android.annotation.SuppressLint
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        removeShiftMode(bottom_navigation_view)
     }
+
+    @SuppressLint("RestrictedApi")
+    private fun removeShiftMode(bottomNavigationView: BottomNavigationView) {
+        val menuView = bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+        try {
+            val mShiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+            mShiftingMode.isAccessible = true
+            mShiftingMode.setBoolean(menuView, false)
+            mShiftingMode.isAccessible = false
+        } catch (e: NoSuchFieldException) {
+            e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        }
+
+        for (i in 0 until menuView.childCount) {
+            val itemView = menuView.getChildAt(i) as BottomNavigationItemView
+            itemView.setShiftingMode(false)
+            itemView.setChecked(itemView.itemData.isChecked)
+        }
+    }
+
 }
