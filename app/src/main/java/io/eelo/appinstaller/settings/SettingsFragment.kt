2d76fa07fd8e80a03ab2a06e8b0ad1d2826eefb9
Nihar Thakkar <nihartar@gmail.com>
2018-08-25 23:16:53 +0530

    Create preferences for settings screen, use new theme colours

--- app/src/main/java/io/eelo/appinstaller/settings/SettingsFragment.kt
+++ app/src/main/java/io/eelo/appinstaller/settings/SettingsFragment.kt
@@ -1,16 +1,19 @@
 package io.eelo.appinstaller.settings
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.preference.PreferenceFragmentCompat
 import io.eelo.appinstaller.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
-        return view
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Create preferences
+        addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        // Empty
     }
 }
