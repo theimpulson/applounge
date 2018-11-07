603438d9e8f53551f0daba27e33af91ba0e58ed7
jo <jo@e.email>
2018-11-07 20:01:57 +0100

    Bug fix: can't execute twice an AsynTask

--- app/src/main/java/io/eelo/appinstaller/categories/activity/model/CategoryModel.kt
+++ app/src/main/java/io/eelo/appinstaller/categories/activity/model/CategoryModel.kt
@@ -2,22 +2,14 @@ package io.eelo.appinstaller.categories.activity.model
 
 import android.arch.lifecycle.MutableLiveData
 import android.content.Context
-import android.os.AsyncTask
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.eelo.appinstaller.application.model.Application
-import io.eelo.appinstaller.application.model.ApplicationData
 import io.eelo.appinstaller.application.model.InstallManager
-import io.eelo.appinstaller.utils.ApplicationParser
 import io.eelo.appinstaller.utils.Common
-import io.eelo.appinstaller.utils.Constants
-import java.net.URL
 
-class CategoryModel : CategoryModelInterface, AsyncTask<Context, Any, ArrayList<Application>>() {
+class CategoryModel : CategoryModelInterface {
 
-    private lateinit var installManager: InstallManager
-    private lateinit var category: String
+    lateinit var installManager: InstallManager
+    lateinit var category: String
     private var page = 1
     val categoryApplicationsList = MutableLiveData<ArrayList<Application>>()
 
@@ -33,26 +25,9 @@ class CategoryModel : CategoryModelInterface, AsyncTask<Context, Any, ArrayList<
     }
 
     override fun loadApplications(context: Context) {
-        executeOnExecutor(Common.EXECUTOR, context)
+        Loader(page, this).executeOnExecutor(Common.EXECUTOR, context)
+        page++
     }
 
-    companion object {
-        private val jsonReader = ObjectMapper().readerFor(CategoryApplicationsResult::class.java)
-    }
-
-    override fun doInBackground(vararg params: Context): ArrayList<Application> {
-        val context = params[0]
-        val result = jsonReader.readValue<CategoryApplicationsResult>(URL(Constants.BASE_URL + "apps?action=list_apps&category=" + category + "&nres=" + Constants.RESULTS_PER_PAGE + "&page=" + page))
-        page++;
-        return ApplicationParser.parseToApps(installManager, context, result.apps)
-    }
-
-    override fun onPostExecute(result: ArrayList<Application>) {
-        categoryApplicationsList.value = result
-    }
-
-    class CategoryApplicationsResult @JsonCreator
-    constructor(@JsonProperty("pages") val pages: Int,
-                @JsonProperty("apps") val apps: Array<ApplicationData>)
 
 }
--- app/src/main/java/io/eelo/appinstaller/categories/model/CategoriesModel.kt
+++ app/src/main/java/io/eelo/appinstaller/categories/model/CategoriesModel.kt
@@ -1,15 +1,9 @@
 package io.eelo.appinstaller.categories.model
 
 import android.arch.lifecycle.MutableLiveData
-import android.os.AsyncTask
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.eelo.appinstaller.utils.Common.EXECUTOR
-import io.eelo.appinstaller.utils.Constants
-import java.net.URL
 
-class CategoriesModel : CategoriesModelInterface, AsyncTask<Any, Any, CategoriesModel.ListCategoriesResult>() {
+class CategoriesModel : CategoriesModelInterface {
     val applicationsCategoriesList = MutableLiveData<ArrayList<Category>>()
     val gamesCategoriesList = MutableLiveData<ArrayList<Category>>()
 
@@ -23,31 +17,6 @@ class CategoriesModel : CategoriesModelInterface, AsyncTask<Any, Any, Categories
     }
 
     override fun loadCategories() {
-        executeOnExecutor(EXECUTOR)
+        Loader(this).executeOnExecutor(EXECUTOR)
     }
-
-    companion object {
-        private val jsonReader = ObjectMapper().readerFor(ListCategoriesResult::class.java)
-    }
-
-    override fun doInBackground(vararg params: Any): ListCategoriesResult {
-        return jsonReader.readValue<ListCategoriesResult>(URL(Constants.BASE_URL + "apps?action=list_cat"))
-    }
-
-    override fun onPostExecute(result: ListCategoriesResult) {
-        val applicationsCategoriesList = ArrayList<Category>()
-        val gamesCategoriesList = ArrayList<Category>()
-        result.appsCategories.forEach { id ->
-            applicationsCategoriesList.add(Category(null, id))
-        }
-        result.gamesCategories.forEach { id ->
-            gamesCategoriesList.add(Category(null, id))
-        }
-        this.applicationsCategoriesList.value = applicationsCategoriesList
-        this.gamesCategoriesList.value = gamesCategoriesList
-    }
-
-    class ListCategoriesResult @JsonCreator
-    constructor(@JsonProperty("apps") val appsCategories: Array<String>,
-                @JsonProperty("games") val gamesCategories: Array<String>)
 }
