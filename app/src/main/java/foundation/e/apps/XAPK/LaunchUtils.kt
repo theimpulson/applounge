79cdb445fbf509d697444ea75314aa56f2f30ce2
Dayona Joseph <dayonajsph8@gmail.com>
2020-06-25 10:05:40 +0000

    Remove installing view for xapk

--- app/src/main/java/foundation/e/apps/XAPK/LaunchUtils.kt
+++ app/src/main/java/foundation/e/apps/XAPK/LaunchUtils.kt
@@ -2,12 +2,19 @@ package foundation.e.apps.XAPK
 
 import android.content.Context
 import android.os.Build
+import android.os.Handler
 import androidx.annotation.RequiresApi
+import foundation.e.apps.application.model.InstallerInterface
 
 object LaunchUtils {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    fun startInstallSplitApksActivity(mActivity: Context, apksBean: ApksBean) {
+    fun startInstallSplitApksActivity(mActivity: Context, apksBean: ApksBean, callback: InstallerInterface) {
         mActivity.startActivity(InstallSplitApksActivity.newInstanceIntent(mActivity, apksBean))
+        val handler = Handler()
+        handler.postDelayed({
+            callback.onInstallationComplete(mActivity)
+        }, 10000)
+
     }
 
 }
