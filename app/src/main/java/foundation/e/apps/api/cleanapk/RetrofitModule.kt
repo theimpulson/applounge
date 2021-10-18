4945e0053a3a1ee202ab538dfcd8bae548b40323
Aayush Gupta <theimpulson@e.email>
2021-10-18 16:00:02 +0530

    Apps: Try to cache something
    Signed-off-by: Aayush Gupta <theimpulson@e.email>

--- app/src/main/java/foundation/e/apps/api/cleanapk/RetrofitModule.kt
+++ app/src/main/java/foundation/e/apps/api/cleanapk/RetrofitModule.kt
@@ -1,9 +1,13 @@
 package foundation.e.apps.api.cleanapk
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -18,11 +22,27 @@ object RetrofitModule {
      */
     @Singleton
     @Provides
-    fun provideCleanAPKInterface(): CleanAPKInterface {
+    fun provideCleanAPKInterface(okHttpClient: OkHttpClient): CleanAPKInterface {
         return Retrofit.Builder()
             .baseUrl(CleanAPKInterface.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(CleanAPKInterface::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCache(@ApplicationContext context: Context): Cache {
+        val cacheSize = (10 * 1024 * 1024).toLong() // 10 MB
+        return Cache(context.cacheDir, cacheSize)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(cache: Cache): OkHttpClient {
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .build()
     }
 }
