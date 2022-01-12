6e1b7bc8f7c6422ba37c012c390a99114b48e83d
Md.Hasib Prince <md.hasibprince@MdHasibs-MacBook-Air.local>
2022-01-12 16:24:44 +0600

    App Lounge: refactoring the method toStringFileSize()

--- app/src/main/java/foundation/e/apps/api/fused/FusedAPIImpl.kt
+++ app/src/main/java/foundation/e/apps/api/fused/FusedAPIImpl.kt
@@ -21,6 +21,7 @@ package foundation.e.apps.api.fused
 import android.app.DownloadManager
 import android.content.Context
 import android.net.Uri
+import android.text.format.Formatter
 import android.util.Log
 import com.aurora.gplayapi.SearchSuggestEntry
 import com.aurora.gplayapi.data.models.App
@@ -473,10 +474,6 @@ class FusedAPIImpl @Inject constructor(
     }
 
     private fun Long.toStringFileSize() : String {
-        if (this <= 0) return "0"
-        val units = arrayOf("B", "kB", "MB", "GB", "TB")
-        val digitGroups = (Math.log10(this.toDouble()) / Math.log10(1024.0)).toInt()
-        return DecimalFormat("#,##0.#").format(this / Math.pow(1024.0, digitGroups.toDouble()))
-            .toString() + " " + units[digitGroups]
+        return Formatter.formatFileSize(context, this)
     }
 }
