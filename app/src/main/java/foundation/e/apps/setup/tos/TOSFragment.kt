ea966d2049c5d94bcfad66e2cd90f21797a3fcf1
TheScarastic <warabhishek@e.email>
2022-02-14 18:04:17 +0530

    App Lounge: Load html and css for tos

--- app/src/main/java/foundation/e/apps/setup/tos/TOSFragment.kt
+++ app/src/main/java/foundation/e/apps/setup/tos/TOSFragment.kt
@@ -1,14 +1,16 @@
 package foundation.e.apps.setup.tos
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.text.Html
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import foundation.e.apps.R
 import foundation.e.apps.databinding.FragmentTosBinding
+import org.jsoup.Jsoup
 
 @AndroidEntryPoint
 class TOSFragment : Fragment(R.layout.fragment_tos) {
@@ -33,17 +35,20 @@ class TOSFragment : Fragment(R.layout.fragment_tos) {
                 binding.TOSButtons.visibility = View.GONE
                 binding.toolbar.visibility = View.VISIBLE
                 binding.acceptDateTV.visibility = View.VISIBLE
+                val constraintSet = ConstraintSet()
+                constraintSet.clone(binding.root)
+                constraintSet.connect(
+                    binding.tosWebView.id,
+                    ConstraintSet.TOP,
+                    binding.acceptDateTV.id,
+                    ConstraintSet.BOTTOM,
+                    20
+                )
+                constraintSet.applyTo(binding.root)
             }
         }
 
-        val tosHtml = resources.openRawResource(R.raw.terms_of_use)
-        val tosHtmlData = tosHtml.reader().readText()
-        binding.TOSTextView.text = Html.fromHtml(
-            tosHtmlData,
-            Html.FROM_HTML_MODE_COMPACT and
-                Html.FROM_HTML_SEPARATOR_LINE_BREAK_HEADING and
-                Html.FROM_HTML_OPTION_USE_CSS_COLORS
-        )
+        loadTos()
 
         binding.toolbar.setNavigationOnClickListener {
             view.findNavController().navigateUp()
@@ -62,5 +67,42 @@ class TOSFragment : Fragment(R.layout.fragment_tos) {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        loadTos()
+    }
+
+    private fun loadTos() {
+        val isNightMode =
+            requireContext().resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK ==
+                Configuration.UI_MODE_NIGHT_YES
+        var css = "terms_of_use.css"
+
+        if (isNightMode) {
+            css = "terms_of_use_dark.css"
+        }
+
+        val tosHtml = resources.openRawResource(R.raw.terms_of_use).reader().readText()
+        val body = Jsoup.parse(tosHtml).body()
+        val sb = StringBuilder()
+            .append(
+                "<HTML><HEAD><LINK href=\"$css\" rel=\"stylesheet\"/></HEAD>"
+            )
+            .append(body.toString())
+            .append("</HTML>")
+        binding.tosWebView.loadDataWithBaseURL(
+            "file:///android_asset/", sb.toString(),
+            "text/html", "utf-8", null
+        )
+
+        binding.tosWebView.setOnScrollChangeListener { _, scrollX, scrollY, _, _ ->
+            if (scrollX == 0 && scrollY == 0 && viewModel.tocStatus.value == true) {
+                binding.acceptDateTV.visibility = View.VISIBLE
+            } else {
+                binding.acceptDateTV.visibility = View.GONE
+            }
+        }
     }
 }
