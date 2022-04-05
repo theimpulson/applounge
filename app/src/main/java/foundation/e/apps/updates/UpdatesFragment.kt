ed6e716cf44697237444fb5f9f7fe760b9e53a05
SayantanRC <sayantan.rc@e.email>
2022-04-05 19:12:53 +0530

    App lounge: (issue_4427) implement FdroidFetchViewModel in app lists and fragments.

--- app/src/main/java/foundation/e/apps/updates/UpdatesFragment.kt
+++ app/src/main/java/foundation/e/apps/updates/UpdatesFragment.kt
@@ -32,6 +32,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import foundation.e.apps.AppProgressViewModel
 import foundation.e.apps.MainActivityViewModel
 import foundation.e.apps.PrivacyInfoViewModel
+import foundation.e.apps.FdroidFetchViewModel
 import foundation.e.apps.R
 import foundation.e.apps.api.fused.FusedAPIInterface
 import foundation.e.apps.api.fused.data.FusedApp
@@ -56,6 +57,7 @@ class UpdatesFragment : Fragment(R.layout.fragment_updates), FusedAPIInterface {
 
     private val updatesViewModel: UpdatesViewModel by viewModels()
     private val privacyInfoViewModel: PrivacyInfoViewModel by viewModels()
+    private val fdroidFetchViewModel: FdroidFetchViewModel by viewModels()
     private val mainActivityViewModel: MainActivityViewModel by activityViewModels()
     private val appProgressViewModel: AppProgressViewModel by viewModels()
 
@@ -83,6 +85,7 @@ class UpdatesFragment : Fragment(R.layout.fragment_updates), FusedAPIInterface {
             ApplicationListRVAdapter(
                 this,
                 privacyInfoViewModel,
+                fdroidFetchViewModel,
                 it,
                 pkgManagerModule,
                 User.valueOf(mainActivityViewModel.userType.value ?: User.UNAVAILABLE.name),
