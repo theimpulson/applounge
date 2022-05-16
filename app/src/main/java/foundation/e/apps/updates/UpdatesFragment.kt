d6800e733c31557dae966e7270421f0a52518d9d
SayantanRC <sayantan.rc@e.email>
2022-05-16 17:58:44 +0530

    App lounge: (issue_5413) implement timeout dialog mechanism in UpdatesFragment

--- app/src/main/java/foundation/e/apps/updates/UpdatesFragment.kt
+++ app/src/main/java/foundation/e/apps/updates/UpdatesFragment.kt
@@ -25,15 +25,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.aurora.gplayapi.data.models.AuthData
 import dagger.hilt.android.AndroidEntryPoint
 import foundation.e.apps.AppInfoFetchViewModel
 import foundation.e.apps.AppProgressViewModel
 import foundation.e.apps.MainActivityViewModel
 import foundation.e.apps.PrivacyInfoViewModel
 import foundation.e.apps.R
+import foundation.e.apps.api.fused.FusedAPIImpl
 import foundation.e.apps.api.fused.FusedAPIInterface
 import foundation.e.apps.api.fused.data.FusedApp
 import foundation.e.apps.application.subFrags.ApplicationDialogFragment
@@ -42,14 +45,17 @@ import foundation.e.apps.databinding.FragmentUpdatesBinding
 import foundation.e.apps.manager.download.data.DownloadProgress
 import foundation.e.apps.manager.pkg.PkgManagerModule
 import foundation.e.apps.updates.manager.UpdatesWorkManager
+import foundation.e.apps.utils.enums.ResultStatus
 import foundation.e.apps.utils.enums.Status
 import foundation.e.apps.utils.enums.User
+import foundation.e.apps.utils.interfaces.TimeoutFragmentInterface
+import foundation.e.apps.utils.modules.CommonUtilsModule.safeNavigate
 import foundation.e.apps.utils.modules.PWAManagerModule
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class UpdatesFragment : Fragment(R.layout.fragment_updates), FusedAPIInterface {
+class UpdatesFragment : Fragment(R.layout.fragment_updates), FusedAPIInterface, TimeoutFragmentInterface {
 
     private var _binding: FragmentUpdatesBinding? = null
     private val binding get() = _binding!!
@@ -74,15 +80,15 @@ class UpdatesFragment : Fragment(R.layout.fragment_updates), FusedAPIInterface {
 
         binding.button.isEnabled = false
 
-        mainActivityViewModel.internetConnection.observe(viewLifecycleOwner) { hasInternet ->
-            mainActivityViewModel.authData.observe(viewLifecycleOwner) { data ->
-                if (hasInternet) {
-                    updatesViewModel.getUpdates(data)
-                    binding.button.setOnClickListener {
-                        UpdatesWorkManager.startUpdateAllWork(requireContext().applicationContext)
-                    }
-                }
-            }
+        /*
+         * Explanation of double observers in HomeFragment.kt
+         */
+
+        mainActivityViewModel.internetConnection.observe(viewLifecycleOwner) {
+            refreshDataOrRefreshToken(mainActivityViewModel)
+        }
+        mainActivityViewModel.authData.observe(viewLifecycleOwner) {
+            refreshDataOrRefreshToken(mainActivityViewModel)
         }
 
         val recyclerView = binding.recyclerView
@@ -140,7 +146,51 @@ class UpdatesFragment : Fragment(R.layout.fragment_updates), FusedAPIInterface {
                 binding.noUpdates.visibility = View.VISIBLE
                 binding.button.isEnabled = false
             }
+            if (it.second != ResultStatus.OK) {
+                onTimeout()
+            }
         }
+    }
+
+    override fun onTimeout() {
+        if (!mainActivityViewModel.isTimeoutDialogDisplayed()) {
+            mainActivityViewModel.displayTimeoutAlertDialog(
+                activity = requireActivity(),
+                message =
+                if (updatesViewModel.getApplicationCategoryPreference() == FusedAPIImpl.APP_TYPE_ANY) {
+                    getString(R.string.timeout_desc_gplay)
+                } else {
+                    getString(R.string.timeout_desc_cleanapk)
+                },
+                positiveButtonText = getString(R.string.retry),
+                positiveButtonBlock = {
+                    showLoadingProgressBar()
+                    mainActivityViewModel.retryFetchingTokenAfterTimeout()
+                },
+                negativeButtonText =
+                if (updatesViewModel.getApplicationCategoryPreference() == FusedAPIImpl.APP_TYPE_ANY) {
+                    getString(R.string.open_settings)
+                } else null,
+                negativeButtonBlock = {
+                    openSettings()
+                },
+                allowCancel = true,
+            )
+        }
+    }
+
+    override fun refreshData(authData: AuthData) {
+        updatesViewModel.getUpdates(authData)
+        binding.button.setOnClickListener {
+            UpdatesWorkManager.startUpdateAllWork(requireContext().applicationContext)
+        }
+    }
+
+    private fun showLoadingProgressBar() {
+        binding.button.isEnabled = false
+        binding.noUpdates.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
+        binding.recyclerView.visibility = View.INVISIBLE
     }
 
     private fun observeDownloadList() {
@@ -188,5 +238,10 @@ class UpdatesFragment : Fragment(R.layout.fragment_updates), FusedAPIInterface {
 
     override fun cancelDownload(app: FusedApp) {
         mainActivityViewModel.cancelDownload(app)
+    }
+
+    private fun openSettings() {
+        view?.findNavController()
+            ?.safeNavigate(R.id.updatesFragment, R.id.action_updatesFragment_to_SettingsFragment)
     }
 }
